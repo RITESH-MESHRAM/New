9f45ca77ea44aa2d6be2d35e88db885ce829ed18
RITESH MESHRAM <126696338+RITESH-MESHRAM@users.noreply.github.com>
2025-06-09 00:51:41 +0530

    Update Class1.java

--- eclipse-workspace/PrcticeGithub/src/package1/Class1.java
+++ eclipse-workspace/PrcticeGithub/src/package1/Class1.java
@@ -4,7 +4,8 @@ public class Class1 {
 
 	public static void main(String[] args) {
 
-System.out.println("this is github project");
+System.out.println("this is github project"); 
+		System.out.println("This code has been changed"); 
 	}
 
 }
